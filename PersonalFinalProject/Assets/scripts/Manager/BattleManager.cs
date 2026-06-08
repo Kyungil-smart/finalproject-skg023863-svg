49aba516251c360249f814a8b077802239089b58
연동준 (플밍 4기) <skg023863@gmail.com>
2026-06-09 08:35:59 +0900

    feat : 기본 공격 애니메이션, 공격 발동 로직 추가

--- PersonalFinalProject/Assets/scripts/Manager/BattleManager.cs
+++ PersonalFinalProject/Assets/scripts/Manager/BattleManager.cs
@@ -1,39 +1,51 @@
 using MyGame;
 using UnityEngine;
 
-public class BattleManager : MonoBehaviour
+namespace MyGame
 {
-    [SerializeField] private InputController _inputController;
-    [SerializeField] private GameObject _player1;
-    [SerializeField] private GameObject _player2;
-    [SerializeField] private FighterData[] _fighterDataList;
-
-    private Fighter _fighter1;
-    private Fighter _fighter2;
-
-    private FighterView _fighter1View;
-    private FighterView _fighter2View;
-
-    void Awake()
+    public class BattleManager : MonoBehaviour
     {
-        _fighter1View = _player1.GetComponent<FighterView>();
-        _fighter2View = _player2.GetComponent<FighterView>();
-        
-        _fighter1 = new Fighter();
-        _fighter2 = new Fighter();
-        
-        _fighter1View.Initialize(_fighter1);
-        _fighter2View.Initialize(_fighter2);
-        _fighter1.BattleSetup(_fighterDataList[0], new Vector2(-2, 0), true);
-        _fighter2.BattleSetup(_fighterDataList[0], new Vector2(2, 0), false);
-    }
+        [SerializeField] private InputController _inputController;
+        [SerializeField] private GameObject _player1;
+        [SerializeField] private GameObject _player2;
+        [SerializeField] private FighterData[] _fighterDataList;
 
-    private void FixedUpdate()
-    {
-        _fighter1.UpdateInput(_inputController.Player1InputData);
-        _fighter2.UpdateInput(_inputController.Player2InputData);
+        private Fighter _fighter1;
+        private Fighter _fighter2;
+
+        private FighterView _fighter1View;
+        private FighterView _fighter2View;
+
+        void Awake()
+        {
+            _fighter1View = _player1.GetComponent<FighterView>();
+            _fighter2View = _player2.GetComponent<FighterView>();
         
-        _fighter1.UpdateMovement();
-        _fighter2.UpdateMovement();
+            _fighter1 = new Fighter();
+            _fighter2 = new Fighter();
+        
+            _fighter1View.Initialize(_fighter1);
+            _fighter2View.Initialize(_fighter2);
+            _fighter1.BattleSetup(_fighterDataList[0], new Vector2(-2, 0), true);
+            _fighter2.BattleSetup(_fighterDataList[0], new Vector2(2, 0), false);
+        }
+
+        private void FixedUpdate()
+        {
+            _fighter1.UpdateInput(_inputController.Player1InputData);
+            _fighter2.UpdateInput(_inputController.Player2InputData);
+            
+            _fighter1.IncrementActionFrame();
+            _fighter2.IncrementActionFrame();
+            
+            _fighter1.UpdateFacingDirection(_fighter2);
+            _fighter2.UpdateFacingDirection(_fighter1);
+            
+            _fighter1.UpdateAction();
+            _fighter2.UpdateAction();
+        
+            _fighter1.UpdateMovement();
+            _fighter2.UpdateMovement();
+        }
     }
 }
